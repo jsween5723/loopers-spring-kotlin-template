5b85b44baee570ca83415be53181236d33f2f58a
jsween5723 <jsween5723@gmail.com>
2025-07-13 20:10:43 +0900

    test(fixture): user integration fixure -> integration fixture로 전환

--- apps/commerce-api/src/test/kotlin/com/loopers/domain/IntegrationTestConfiguration.kt
+++ apps/commerce-api/src/test/kotlin/com/loopers/domain/IntegrationTestConfiguration.kt
@@ -1,12 +1,11 @@
 package com.loopers.domain
 
 import com.loopers.application.user.UserService
-import com.loopers.domain.user.UserIntegrationTestFixture
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 
 @TestConfiguration
 class IntegrationTestConfiguration {
     @Bean
-    fun userIntegrationTestFixture(userService: UserService): UserIntegrationTestFixture = UserIntegrationTestFixture(userService)
+    fun userIntegrationTestFixture(userService: UserService): IntegrationTestFixture = IntegrationTestFixture(userService)
 }
--- apps/commerce-api/src/test/kotlin/com/loopers/domain/IntegrationTestFixture.kt
+++ apps/commerce-api/src/test/kotlin/com/loopers/domain/IntegrationTestFixture.kt
@@ -1,9 +1,12 @@
-package com.loopers.domain.user
+package com.loopers.domain
 
 import com.loopers.application.user.UserService
+import com.loopers.domain.user.User
+import com.loopers.domain.user.UserCommand
+import com.loopers.domain.user.UserCommandGenerator
 import org.springframework.beans.factory.annotation.Autowired
 
-class UserIntegrationTestFixture(@Autowired val userService: UserService) {
+class IntegrationTestFixture(@Autowired val userService: UserService) {
 
     fun 회원가입(command: UserCommand.Create = UserCommandGenerator.Create()): User = userService.create(command)
 
