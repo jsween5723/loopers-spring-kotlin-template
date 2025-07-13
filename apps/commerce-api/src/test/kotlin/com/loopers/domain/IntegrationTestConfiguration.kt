b91b8fb5d5df444ccbf854e2d118b168a0cc58ac
jsween5723 <jsween5723@gmail.com>
2025-07-13 20:43:09 +0900

    test(point): 해당 ID 의 회원이 존재할 경우, 보유 포인트가 반환된다. (성공)

--- apps/commerce-api/src/test/kotlin/com/loopers/domain/IntegrationTestConfiguration.kt
+++ apps/commerce-api/src/test/kotlin/com/loopers/domain/IntegrationTestConfiguration.kt
@@ -1,11 +1,15 @@
 package com.loopers.domain
 
 import com.loopers.application.user.UserService
+import com.loopers.domain.point.UserPointService
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 
 @TestConfiguration
 class IntegrationTestConfiguration {
     @Bean
-    fun userIntegrationTestFixture(userService: UserService): IntegrationTestFixture = IntegrationTestFixture(userService)
+    fun userIntegrationTestFixture(
+        userService: UserService,
+        userPointService: UserPointService,
+    ): IntegrationTestFixture = IntegrationTestFixture(userService, userPointService)
 }
--- apps/commerce-api/src/test/kotlin/com/loopers/domain/point/UserPointIntegrationTest.kt
+++ apps/commerce-api/src/test/kotlin/com/loopers/domain/point/UserPointIntegrationTest.kt
@@ -7,6 +7,7 @@ import com.loopers.domain.IntegrationTestFixture.Companion.NO_EXIST_USER_ID
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
+import kotlin.test.assertEquals
 
 @IntegrationTest
 class UserPointIntegrationTest {
@@ -19,5 +20,20 @@ class UserPointIntegrationTest {
         val command = UserPointCommandGenerator.Charge(userId = NO_EXIST_USER_ID)
         // act
         assertThrows<NoSuchElementException> { userPointFacade.charge(command) }
+    }
+
+    @Test
+    fun `조회 시 해당 ID 의 회원이 존재할 경우, 보유 포인트가 반환된다`(@Autowired fixture: IntegrationTestFixture) {
+        // arrange
+        val 사용자 = fixture.기본_사용자_등록()
+        val 충전된_포인트 = fixture.충전하기()
+        // act
+        val myPoint = userPointFacade.getMe(userId = 사용자.id)
+        // assert
+        assertEquals(
+            0,
+            충전된_포인트.point
+        .compareTo(requireNotNull(myPoint?.point)),
+        )
     }
 }
