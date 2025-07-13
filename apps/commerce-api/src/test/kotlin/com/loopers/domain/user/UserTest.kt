03e6cef72c03c4691ac04cbf101ac03d0cdc47b2
jsween5723 <jsween5723@gmail.com>
2025-07-13 12:06:11 +0900

    test(user): 이메일이 xx@yy.zz 형식에 맞지 않으면, User 객체 생성에 실패한다.

--- apps/commerce-api/src/test/kotlin/com/loopers/domain/user/UserTest.kt
+++ apps/commerce-api/src/test/kotlin/com/loopers/domain/user/UserTest.kt
@@ -1,5 +1,6 @@
 package com.loopers.domain.user
 
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -19,6 +20,26 @@ class UserTest {
     fun `ID 가 영문 및 숫자 10자 이내 형식에 맞지 않으면, User 객체 생성에 실패한다`(username: String) {
         // arrange
         val command = UserCommandGenerator.Create(username = username)
+        // act
+        // assert
+        assertThrows<IllegalArgumentException> { command.toUser() }
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+        strings = [
+            "invalidemail",
+            "invalid@",
+            "",
+            "invalid@example",
+            "invalid@.com",
+            "invalid@example.",
+        ],
+    )
+    @DisplayName("이메일이 xx@yy.zz 형식에 맞지 않으면, User 객체 생성에 실패한다.")
+    fun `이메일이 xx@yy_zz 형식에 맞지 않으면, User 객체 생성에 실패한다`(email: String) {
+        // arrange
+        val command = UserCommandGenerator.Create(email = email)
         // act
         // assert
         assertThrows<IllegalArgumentException> { command.toUser() }
