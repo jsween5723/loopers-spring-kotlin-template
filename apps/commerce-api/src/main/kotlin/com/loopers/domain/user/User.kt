43da27fa95298abd33380aac61378b66103dfcc6
jsween5723 <jsween5723@gmail.com>
2025-07-13 12:10:08 +0900

    feat(user): 이메일이 xx@yy.zz 형식에 맞지 않으면, User 객체 생성에 실패한다.

--- apps/commerce-api/src/main/kotlin/com/loopers/domain/user/User.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/domain/user/User.kt
@@ -9,9 +9,11 @@ class User(@NaturalId val username: String, val name: String, val email: String,
     BaseEntity() {
     init {
         require(USERNAME_REGEX.matches(username)) { "${username}이 영문 숫자 10자이내를 만족하지않습니다." }
+        require(EMAIL_REGEX.matches(email)) { "${email}은 올바른 이메일 형식이 아닙니다." }
     }
 
     companion object {
         private val USERNAME_REGEX = "^[A-Za-z0-9]{1,9}\$".toRegex()
+        private val EMAIL_REGEX = "^[A-Za-z0-9._%+\\-]+@[A-Za-z0-9.\\-]+\\.[A-Za-z]{2,}\$".toRegex()
     }
 }
