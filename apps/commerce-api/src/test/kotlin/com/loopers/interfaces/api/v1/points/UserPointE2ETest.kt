fe0e0f79a4ddda639650d9c938478a2b05bc6d7f
jsween5723 <jsween5723@gmail.com>
2025-07-13 19:37:45 +0900

    style(point): 클래스 prefix UserPoint로 변경

--- apps/commerce-api/src/test/kotlin/com/loopers/interfaces/api/v1/points/UserPointE2ETest.kt
+++ apps/commerce-api/src/test/kotlin/com/loopers/interfaces/api/v1/points/UserPointE2ETest.kt
@@ -10,12 +10,12 @@ import org.springframework.http.ProblemDetail
 import kotlin.test.assertEquals
 
 @ApiTest
-class PointE2ETest {
+class UserPointE2ETest {
     @Test
     fun `존재하는 유저가 1000원을 충전할 경우, 충전된 보유 총량을 응답으로 반환한다`(@Autowired fixture: ApiTestFixture) {
         // arrange
         fixture.기본_사용자_지정()
-        val request = PointRequestGenerator.Charge(amount = 1000.toBigDecimal())
+        val request = UserPointRequestGenerator.Charge(amount = 1000.toBigDecimal())
         // act
         val result = fixture.testRestTemplate.patchForObject<UserPointResponse.Charge>(POINT_CHARGE_URI, request)
         // arrange
@@ -25,7 +25,7 @@ class PointE2ETest {
     @Test
     fun `X-USER-ID 헤더가 없을 경우, 400 Bad Request 응답을 반환한다`(@Autowired fixture: ApiTestFixture) {
         // arrange
-        val request = PointRequestGenerator.Charge(amount = 400.toBigDecimal())
+        val request = UserPointRequestGenerator.Charge(amount = 400.toBigDecimal())
         // act
         val result = fixture.testRestTemplate.patchForObject<ProblemDetail>(POINT_CHARGE_URI, request)
         // assert
