0e494a141c1eda8bf7f57108aefb65c15689a738
jsween5723 <jsween5723@gmail.com>
2025-07-13 20:53:26 +0900

    feat(point): 0 이하의 정수로 포인트를 충전 시 실패한다.

--- apps/commerce-api/src/main/kotlin/com/loopers/domain/point/UserPoint.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/domain/point/UserPoint.kt
@@ -8,6 +8,7 @@ import java.math.BigDecimal
 @Entity(name = "user_points")
 class UserPoint(var point: BigDecimal = BigDecimal.ZERO, @NaturalId val userId: Long) : BaseEntity() {
     fun charge(amount: BigDecimal) {
+        require(amount >= BigDecimal.ZERO) { "충전 금액은 0 이상이어야 합니다." }
         point = point.add(amount)
     }
 }
