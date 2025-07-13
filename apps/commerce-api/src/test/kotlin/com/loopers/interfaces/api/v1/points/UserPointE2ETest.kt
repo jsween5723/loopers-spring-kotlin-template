10c201642d931759e608e93c29e2c2590e640e86
jsween5723 <jsween5723@gmail.com>
2025-07-13 19:42:24 +0900

    test(point): 존재하지 않는 유저로 요청할 경우, 404 Not Found 응답을 반환한다

--- apps/commerce-api/src/test/kotlin/com/loopers/interfaces/api/v1/points/UserPointE2ETest.kt
+++ apps/commerce-api/src/test/kotlin/com/loopers/interfaces/api/v1/points/UserPointE2ETest.kt
@@ -3,6 +3,7 @@ package com.loopers.interfaces.api.v1.points
 import com.loopers.interfaces.api.v1.ApiTest
 import com.loopers.interfaces.api.v1.ApiTestFixture
 import com.loopers.interfaces.api.v1.ApiTestFixture.Companion.POINT_CHARGE_URI
+import com.loopers.interfaces.api.v1.ApiTestFixture.Companion.UNAVAILABLE_USER_ID
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.web.client.patchForObject
@@ -18,8 +19,19 @@ class UserPointE2ETest {
         val request = UserPointRequestGenerator.Charge(amount = 1000.toBigDecimal())
         // act
         val result = fixture.testRestTemplate.patchForObject<UserPointResponse.Charge>(POINT_CHARGE_URI, request)
-        // arrange
+        // assert
         assertEquals(0, 1000.toBigDecimal().compareTo(requireNotNull(result?.point)))
+    }
+
+    @Test
+    fun `존재하지 않는 유저로 요청할 경우, 404 Not Found 응답을 반환한다`(@Autowired fixture: ApiTestFixture) {
+        // arrange
+        fixture.사용자_지정(UNAVAILABLE_USER_ID)
+        val request = UserPointRequestGenerator.Charge()
+        // act
+        val result = fixture.testRestTemplate.patchForObject<ProblemDetail>(POINT_CHARGE_URI, request)
+        // assert
+        assertEquals(404, requireNotNull(result?.status))
     }
 
     @Test
