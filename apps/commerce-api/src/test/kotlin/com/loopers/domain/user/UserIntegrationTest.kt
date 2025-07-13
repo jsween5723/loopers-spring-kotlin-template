b0cd5c32bd86f9c259a38284e9d70a9fc62e7448
jsween5723 <jsween5723@gmail.com>
2025-07-13 11:20:36 +0900

    test(user): 이미 가입된 ID 로 회원가입 시도 시, 실패한다

--- apps/commerce-api/src/test/kotlin/com/loopers/domain/user/UserIntegrationTest.kt
+++ apps/commerce-api/src/test/kotlin/com/loopers/domain/user/UserIntegrationTest.kt
@@ -1,11 +1,12 @@
 package com.loopers.domain.user
 
-import com.loopers.application.user.UserService
 import com.loopers.domain.IntegrationTest
 import com.ninjasquad.springmockk.SpykBean
 import io.mockk.verify
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.dao.DataIntegrityViolationException
 import kotlin.test.assertNotNull
 
 @IntegrationTest
@@ -13,17 +14,23 @@ class UserIntegrationTest {
     @SpykBean
     private lateinit var repository: UserRepository
 
-    @Autowired
-    private lateinit var userService: UserService
-
     @Test
-    fun `회원 가입시 User 저장이 수행된다`() {
+    fun `회원 가입시 User 저장이 수행된다`(@Autowired fixture: UserIntegrationTestFixture) {
         // arrange
         val command = UserCommandGenerator.Create()
         // act
-        val user = userService.create(command)
+        val user = fixture.userService.create(command)
         // assert
         verify { repository.save(any()) }
         assertNotNull(user.id)
+    }
+
+    @Test
+    fun `이미 가입된 ID 로 회원가입 시도 시, 실패한다`(@Autowired fixture: UserIntegrationTestFixture) {
+        // arrange
+        val 가입된_사용자 = fixture.회원가입()
+        // act
+        // assert
+        assertThrows<DataIntegrityViolationException> { fixture.userService.create(UserCommandGenerator.Create(username = 가입된_사용자.username)) }
     }
 }
---
+++ apps/commerce-api/src/test/kotlin/com/loopers/domain/user/UserIntegrationTestFixture.kt
@@ -0,0 +1,9 @@
+package com.loopers.domain.user
+
+import com.loopers.application.user.UserService
+import org.springframework.beans.factory.annotation.Autowired
+
+class UserIntegrationTestFixture(@Autowired val userService: UserService) {
+
+    fun 회원가입(command: UserCommand.Create = UserCommandGenerator.Create()): User = userService.create(command)
+}
