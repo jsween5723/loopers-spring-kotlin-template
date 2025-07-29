fea81e43878befededf8397df33a01a181c31b00
jsween5723 <jsween5723@gmail.com>
2025-07-29 19:18:12 +0900

    fix(brand): 브랜드 초기상태 변경

--- apps/commerce-api/src/main/kotlin/com/loopers/domain/brand/Brand.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/domain/brand/Brand.kt
@@ -5,7 +5,7 @@ import jakarta.persistence.Entity
 
 @Entity
 class Brand(val name: String) : BaseEntity() {
-    var state = State.CLOSED
+    var state = State.OPENED
 
     fun isAvailable(): Boolean = state == State.OPENED
     enum class State {
