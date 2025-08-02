a1befcda4791ff5fabe5c292052e5716464d47d5
jsween5723 <jsween5723@gmail.com>
2025-08-02 23:15:49 +0900

    feat(point): 포인트 UserId 추가

--- apps/commerce-api/src/main/kotlin/com/loopers/domain/point/UserPoint.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/domain/point/UserPoint.kt
@@ -1,14 +1,40 @@
 package com.loopers.domain.point
 
 import com.loopers.domain.BaseEntity
+import com.loopers.domain.payment.Paid
+import com.loopers.domain.payment.Payment
+import com.loopers.domain.payment.PaymentInstrument
+import com.loopers.domain.user.UserId
 import jakarta.persistence.Entity
 import org.hibernate.annotations.NaturalId
 import java.math.BigDecimal
 
 @Entity(name = "user_points")
-class UserPoint(var point: BigDecimal = BigDecimal.ZERO, @NaturalId val userId: Long) : BaseEntity() {
+class UserPoint(
+    var point: BigDecimal = BigDecimal.ZERO,
+    @NaturalId
+    val userId: UserId,
+) : BaseEntity(),
+    PaymentInstrument {
     fun charge(amount: BigDecimal) {
         require(amount >= BigDecimal.ZERO) { "충전 금액은 0 이상이어야 합니다." }
         point = point.add(amount)
+    }
+
+    private fun use(amount: BigDecimal) {
+        require(amount <= point) { "사용 금액은 보유량 이하여야 합니다." }
+        point = point.subtract(amount)
+    }
+
+    override val type: PaymentInstrument.Type
+        get() = PaymentInstrument.Type.USER_POINT
+
+    override fun pay(amount: BigDecimal, userId: UserId): Paid {
+        use(amount)
+        return Payment.paid(
+            target = amount,
+            instrument = this,
+            userId = userId,
+        )
     }
 }
