1217d0ebf8eee527af9641aaa5b9da149e44bb41
jsween5723 <jsween5723@gmail.com>
2025-08-03 00:05:08 +0900

    refactor(product-like): 상품 좋아요 리팩터링

--- apps/commerce-api/src/test/kotlin/com/loopers/application/product/ProductLikeFacadeTest.kt
+++ apps/commerce-api/src/test/kotlin/com/loopers/application/product/ProductLikeFacadeTest.kt
@@ -3,6 +3,8 @@ package com.loopers.application.product
 import com.loopers.domain.IntegrationTest
 import com.loopers.domain.product.Product
 import com.loopers.domain.product.ProductRepository
+import com.loopers.domain.product.ProductSignal
+import com.loopers.domain.product.ProductSignalRepository
 import com.loopers.domain.productlike.ProductLike
 import com.loopers.domain.productlike.ProductLikeRepository
 import com.loopers.domain.user.UserId
@@ -22,6 +24,9 @@ class ProductLikeFacadeTest {
     @Autowired
     private lateinit var productRepository: ProductRepository
 
+    @Autowired
+    private lateinit var productSignalRepository: ProductSignalRepository
+
     val product: Product = Product(name = "Jerry Paul", brandId = 1L, displayedAt = ZonedDateTime.now(), maxQuantity = 7327, price = 2000.toBigDecimal(), stock = 2556)
     val userId: UserId = UserId(2L)
 
@@ -29,6 +34,7 @@ class ProductLikeFacadeTest {
     fun `좋아요 추가에 성공한다`() {
         // arrange
         val product = productRepository.save(product)
+        productSignalRepository.save(ProductSignal(product, 0))
         // act
         // assert
         assertDoesNotThrow { sut.add(userId, product.id) }
@@ -38,6 +44,7 @@ class ProductLikeFacadeTest {
     fun `이미 좋아요했어도 아무동작하지 않고 성공한다`() {
         // arrange
         val product = productRepository.save(product)
+        productSignalRepository.save(ProductSignal(product, 0))
         productLikeRepository.save(ProductLike(product = product, userId = userId))
         // act
         // assert
@@ -48,6 +55,7 @@ class ProductLikeFacadeTest {
     fun `좋아요 삭제에 성공한다`() {
         // arrange
         val product = productRepository.save(product)
+        productSignalRepository.save(ProductSignal(product, 0))
         sut.add(userId, product.id)
         // act
         // assert
@@ -58,6 +66,7 @@ class ProductLikeFacadeTest {
     fun `좋아요가 없어도 아무동작하지 않고 성공한다`() {
         // arrange
         val product = productRepository.save(product)
+        productSignalRepository.save(ProductSignal(product, 0))
         // act
         // assert
         assertDoesNotThrow { sut.remove(userId, product.id) }
