3310edc668bd66c8122016e671e29eb0e3f49465
jsween5723 <jsween5723@gmail.com>
2025-08-04 10:40:11 +0900

    feat(order): 동일한 유저가 서로 다른 주문을 동시에 수행해도, 포인트가 정상적으로 차감되어야 한다

--- apps/commerce-api/src/test/kotlin/com/loopers/application/order/OrderPayFacadeTest.kt
+++ apps/commerce-api/src/test/kotlin/com/loopers/application/order/OrderPayFacadeTest.kt
@@ -1,5 +1,6 @@
 package com.loopers.application.order
 
+import com.loopers.concurrency
 import com.loopers.domain.IntegrationTest
 import com.loopers.domain.payment.Payment
 import com.loopers.domain.payment.PaymentMethod
@@ -79,7 +80,7 @@ class OrderPayFacadeTest(
         assertThat(payResult.payments[0].amount).isEqualTo(create.totalPrice)
         // 반영확인
         // TODO: 쿠폰 반영 확인
-        val actualPoint = userPointJpaRepository.findByUserId(userId = userId)!!
+        val actualPoint = userPointJpaRepository.findForUpdateByUserId(userId = userId)!!
         assertThat(actualPoint.point).isEqualByComparingTo(Long.MAX_VALUE.toBigDecimal() - 1000.toBigDecimal())
         val productActual = productJpaRepository.findByIdOrNull(product.id)!!
         assertThat(productActual.stock).isEqualTo(0)
@@ -106,7 +107,7 @@ class OrderPayFacadeTest(
         assertThatThrownBy { orderPayFacade.pay(userId, criteria = criteria) }.isInstanceOf(IllegalStateException::class.java)
         // 롤백처리 확인
         // TODO: 쿠폰 롤백처리 확인
-        val actualPoint = userPointJpaRepository.findByUserId(userId)!!
+        val actualPoint = userPointJpaRepository.findForUpdateByUserId(userId)!!
         assertThat(actualPoint.point).isEqualByComparingTo(Long.MAX_VALUE.toBigDecimal())
     }
 
@@ -134,5 +135,33 @@ class OrderPayFacadeTest(
         // TODO: 쿠폰 롤백처리 확인
         val actualProduct = productJpaRepository.findByIdOrNull(product.id)!!
         assertThat(actualProduct.stock).isEqualTo(1)
+    }
+
+    @Test
+    fun `동일한 유저가 서로 다른 주문을 동시에 수행해도, 포인트가 정상적으로 차감되어야 한다`() {
+        // arrange
+        val userId = UserId(2L)
+        val userPoint = userPointJpaRepository.save(UserPoint(userId = userId, point = BigDecimal.valueOf(Long.MAX_VALUE)))
+        val stock = 2L
+        val price = 20000.toBigDecimal()
+        val product = productJpaRepository.save(Product(name = "Miranda Moore", brandId = 5968, displayedAt = ZonedDateTime.now(), maxQuantity = 2, price = price, stock = stock))
+        val criteriaList = IntRange(1, 2).map { orderCreateFacade.create(userId, listOf(IdAndQuantity(productId = product.id, quantity = 1))) }
+            .map {
+                OrderPayFacade.Criteria(
+                    orderId = it.id,
+                    targets = listOf(
+                        OrderPayFacade.Criteria.PaymentMethodTypeAndAmount(
+                            type = PaymentMethod.Type.USER_POINT,
+                            amount = it.totalPrice,
+                        ),
+                    ),
+                )
+            }
+        val acts = criteriaList.map { { sut.pay(userId, it) } }
+        // act
+        concurrency(acts)
+        // assert
+        val actual = userPointJpaRepository.findByIdOrNull(userPoint.id)!!
+        assertThat(actual.point).isEqualByComparingTo(Long.MAX_VALUE.toBigDecimal() - 40000.toBigDecimal())
     }
 }
