2d106ea8f7ee8431bad6e6af6b3c43ac1c1971a5
jsween5723 <jsween5723@gmail.com>
2025-08-22 16:16:44 +0900

    feat(payment): 요청 시 retry 및 폴백 추가

--- apps/commerce-api/src/main/kotlin/com/loopers/infrastructure/payment/PaymentGateway.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/infrastructure/payment/PaymentGateway.kt
@@ -1,6 +1,8 @@
 package com.loopers.infrastructure.payment
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
+import io.github.resilience4j.retry.annotation.Retry
+import org.hibernate.query.sqm.tree.SqmNode.log
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,16 +14,30 @@ import org.springframework.web.service.annotation.GetExchange
 import org.springframework.web.service.annotation.HttpExchange
 import org.springframework.web.service.annotation.PostExchange
 import org.springframework.web.service.invoker.HttpServiceProxyFactory
+import java.util.UUID
 
 @HttpExchange(url = "/api/v1/payments")
 interface PaymentGateway {
     @PostExchange
     @CircuitBreaker(name = "pg-get")
+    @Retry(name = "pg-request", fallbackMethod = "requestPaymentFallback")
     fun request(@RequestBody request: PaymentDto.PaymentRequest): PaymentDto.TransactionResponse
 
     @GetExchange("/{transactionKey}")
     @CircuitBreaker(name = "pg-get")
     fun getTransaction(@PathVariable transactionKey: String): PaymentDto.TransactionDetailResponse
+
+    private fun requestPaymentFallback(
+       request: PaymentDto.PaymentRequest,
+        ex: Throwable,
+    ): PaymentDto.TransactionResponse {
+        log.error("PG createPayment failed $request", ex)
+        return PaymentDto.TransactionResponse(
+            transactionKey = UUID.randomUUID().toString(),
+            status = PaymentDto.TransactionStatusResponse.FAILED,
+            reason = null,
+        )
+    }
 }
 
 @Configuration
